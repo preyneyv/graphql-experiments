7774a3576ac84567349deabe2f34974d3262fce4
Pranav Nutalapati <pranavnutalapati@gmail.com>
2020-12-10 20:25:12 +0530

    Set up bundling for editor, with compilation directives for UMD and NPM

---
+++ packages/ide/src/cdn.tsx
@@ -0,0 +1,7 @@
+import './style.scss'
+
+import React from 'react'
+
+export const App = () => {
+    return <h1>Hello from Pranav!</h1>
+}
---
+++ packages/ide/webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path')
+const { WebpackPluginServe } = require('webpack-plugin-serve')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isHMR = Boolean(isDev && process.env.WEBPACK_DEV_SERVER)
+
+const rootPath = (...args) => path.resolve(__dirname, ...args)
+
+module.exports = {
+    mode: process.env.NODE_ENV,
+    entry: './src/cdn.tsx',
+    context: rootPath(),
+    output: {
+        path: rootPath(),
+        library: 'experimentsIDE',
+        libraryTarget: 'umd',
+        filename: isDev ? 'experiments-ide.js' : 'experiments-ide.min.js',
+    },
+    devtool: isDev ? 'eval-cheap-module-source-map' : 'source-map',
+    externals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|ts|tsx)$/,
+                use: [{ loader: 'babel-loader' }],
+                exclude: /\.(d\.ts|d\.ts\.map|spec\.tsx)$/
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: isHMR,
+                        },
+                    },
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: rootPath('resources', 'index.html'),
+            inject: 'head',
+            scriptLoading: 'blocking',
+            filename: isDev ? 'index.html' : 'index.min.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: isDev ? 'experiments-ide.css' : 'experiments-ide.min.css',
+            chunkFilename: '[id].css'
+        }),
+    ]
+}
