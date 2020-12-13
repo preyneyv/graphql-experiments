4e2af0124442190e4f3f16396bb353809d492902
Pranav Nutalapati <pranavnutalapati@gmail.com>
2020-12-13 14:54:23 +0530

    Fixed .babelrc to properly compile Typescript namespaces and started work on implementing the configuration modal.

--- packages/ide/src/cdn.tsx
+++ packages/ide/src/cdn.tsx
@@ -1,5 +1,6 @@
-// import './css/style.scss'
+import './css/index.scss'
+import './css/modals.scss'
 import 'codemirror/lib/codemirror.css'
 // import 'codemirror/theme/material.css'
 
-export { ExperimentsIDE } from './components/experiments-ide'
+export { ExperimentsIDE } from './components/ExperimentsIDE'
